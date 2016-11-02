359cb10b337e302f3d7eae612aa94bb04a4279e1
AdamRakaska <arakaska@gmail.com>
2016-11-02 15:10:51 -0600

    Hid less-useful entropy measures

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -33,16 +33,10 @@
             this.tbShannonSpecific = new System.Windows.Forms.TextBox();
             this.tbShannonNormalized = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.tbAbsolute = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.tbAbsoluteNormalized = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.tbCompression = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.progressBarEntropy = new System.Windows.Forms.ProgressBar();
             this.progressBarCompression = new System.Windows.Forms.ProgressBar();
@@ -52,6 +46,12 @@
             this.tbFilePath = new System.Windows.Forms.TextBox();
             this.btnByteDetails = new System.Windows.Forms.Button();
             this.btnFileDetails = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbAbsolute = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbAbsoluteNormalized = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -101,46 +101,10 @@
             this.label2.Text = "Shannon Specific Entropy (Normalized):";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
-            // tbAbsolute
-            // 
-            this.tbAbsolute.Enabled = false;
-            this.tbAbsolute.Location = new System.Drawing.Point(204, 50);
-            this.tbAbsolute.Name = "tbAbsolute";
-            this.tbAbsolute.Size = new System.Drawing.Size(100, 20);
-            this.tbAbsolute.TabIndex = 6;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(108, 53);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Absolute Entropy:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // tbAbsoluteNormalized
-            // 
-            this.tbAbsoluteNormalized.Enabled = false;
-            this.tbAbsoluteNormalized.Location = new System.Drawing.Point(204, 72);
-            this.tbAbsoluteNormalized.Name = "tbAbsoluteNormalized";
-            this.tbAbsoluteNormalized.Size = new System.Drawing.Size(100, 20);
-            this.tbAbsoluteNormalized.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(47, 75);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(151, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Absolute Entropy (Normalized):";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
             // tbCompression
             // 
             this.tbCompression.Enabled = false;
-            this.tbCompression.Location = new System.Drawing.Point(204, 94);
+            this.tbCompression.Location = new System.Drawing.Point(204, 50);
             this.tbCompression.Name = "tbCompression";
             this.tbCompression.Size = new System.Drawing.Size(100, 20);
             this.tbCompression.TabIndex = 10;
@@ -148,7 +112,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(100, 97);
+            this.label5.Location = new System.Drawing.Point(100, 53);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 13);
             this.label5.TabIndex = 9;
@@ -173,28 +137,10 @@
             this.label7.TabIndex = 12;
             this.label7.Text = "entropy/symbol (Range: 0 to 1)";
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(310, 53);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(23, 13);
-            this.label8.TabIndex = 13;
-            this.label8.Text = "bits";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(310, 75);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(42, 13);
-            this.label9.TabIndex = 14;
-            this.label9.Text = "entropy";
-            // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(310, 97);
+            this.label10.Location = new System.Drawing.Point(310, 53);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(174, 13);
             this.label10.TabIndex = 15;
@@ -204,7 +150,7 @@
             // 
             this.progressBarEntropy.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBarEntropy.Location = new System.Drawing.Point(5, 216);
+            this.progressBarEntropy.Location = new System.Drawing.Point(5, 165);
             this.progressBarEntropy.Maximum = 800;
             this.progressBarEntropy.Name = "progressBarEntropy";
             this.progressBarEntropy.Size = new System.Drawing.Size(629, 20);
@@ -214,7 +160,7 @@
             // 
             this.progressBarCompression.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBarCompression.Location = new System.Drawing.Point(5, 179);
+            this.progressBarCompression.Location = new System.Drawing.Point(5, 128);
             this.progressBarCompression.Name = "progressBarCompression";
             this.progressBarCompression.Size = new System.Drawing.Size(629, 20);
             this.progressBarCompression.TabIndex = 17;
@@ -223,7 +169,7 @@
             // 
             this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(0, 201);
+            this.label11.Location = new System.Drawing.Point(0, 150);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(91, 13);
             this.label11.TabIndex = 18;
@@ -233,7 +179,7 @@
             // 
             this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(0, 164);
+            this.label12.Location = new System.Drawing.Point(0, 113);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(93, 13);
             this.label12.TabIndex = 19;
@@ -241,24 +187,24 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.tbShannonSpecific);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.tbShannonNormalized);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label10);
             this.panel1.Controls.Add(this.tbAbsolute);
             this.panel1.Controls.Add(this.label9);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.tbAbsoluteNormalized);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbShannonSpecific);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tbShannonNormalized);
+            this.panel1.Controls.Add(this.label10);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.tbCompression);
             this.panel1.Location = new System.Drawing.Point(5, 33);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(488, 120);
+            this.panel1.Size = new System.Drawing.Size(488, 74);
             this.panel1.TabIndex = 20;
             // 
             // tbFilePath
@@ -293,11 +239,71 @@
             this.btnFileDetails.UseVisualStyleBackColor = true;
             this.btnFileDetails.Click += new System.EventHandler(this.btnFileDetails_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Absolute Entropy:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.Visible = false;
+            // 
+            // tbAbsolute
+            // 
+            this.tbAbsolute.Enabled = false;
+            this.tbAbsolute.Location = new System.Drawing.Point(204, 72);
+            this.tbAbsolute.Name = "tbAbsolute";
+            this.tbAbsolute.Size = new System.Drawing.Size(100, 20);
+            this.tbAbsolute.TabIndex = 17;
+            this.tbAbsolute.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(310, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "entropy";
+            this.label9.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Absolute Entropy (Normalized):";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label4.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(310, 75);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "bits";
+            this.label8.Visible = false;
+            // 
+            // tbAbsoluteNormalized
+            // 
+            this.tbAbsoluteNormalized.Enabled = false;
+            this.tbAbsoluteNormalized.Location = new System.Drawing.Point(204, 94);
+            this.tbAbsoluteNormalized.Name = "tbAbsoluteNormalized";
+            this.tbAbsoluteNormalized.Size = new System.Drawing.Size(100, 20);
+            this.tbAbsoluteNormalized.TabIndex = 19;
+            this.tbAbsoluteNormalized.Visible = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 242);
+            this.ClientSize = new System.Drawing.Size(639, 191);
             this.Controls.Add(this.btnFileDetails);
             this.Controls.Add(this.btnByteDetails);
             this.Controls.Add(this.tbFilePath);
@@ -326,16 +332,10 @@
         private System.Windows.Forms.TextBox tbShannonSpecific;
         private System.Windows.Forms.TextBox tbShannonNormalized;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbAbsolute;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox tbAbsoluteNormalized;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox tbCompression;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.ProgressBar progressBarEntropy;
         private System.Windows.Forms.ProgressBar progressBarCompression;
@@ -345,6 +345,12 @@
         private System.Windows.Forms.TextBox tbFilePath;
         private System.Windows.Forms.Button btnByteDetails;
         private System.Windows.Forms.Button btnFileDetails;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbAbsolute;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbAbsoluteNormalized;
     }
 }
 
